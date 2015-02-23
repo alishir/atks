275ca2e2499392f4dde97ba0c270f368bbeaf697
Ali Shirvani Jouzdani <aj.shirvani@gmail.com>
2015-02-24 01:31:53 +0330

    from given aya to first aya of next page, aya number is current

--- app/controllers/quran_controller.rb
+++ app/controllers/quran_controller.rb
@@ -5,14 +5,15 @@ class QuranController < ApplicationController
     quran_data = Nokogiri::XML(File.open("#{root}/lib/quran-data.xml"))
 
     # retrieve sura number and reqAya from user request
-    sura = 4
+    reqSura = 4
     reqAya = 32
-    @page = quran_data.search("page[sura=\"#{sura}\"]").select{|page| page['aya'].to_i <= reqAya}.last['index']
+    @page = quran_data.search("page[sura=\"#{reqSura}\"]").select{|page| page['aya'].to_i <= reqAya}.last['index']
     nextPage = @page.to_i + 1
     @nextPageSura = quran_data.search("page[index=\"#{nextPage}\"]").first['sura']
     @nextPageAya = quran_data.search("page[index=\"#{nextPage}\"]").first['aya']
 
-    sura1 = quran.search("sura[index=\"#{sura}\"]")
-    @ayat = sura1.xpath('aya/@text')
+    sura1 = quran.search("sura[index=\"#{reqSura}\"]")
+    @sura = reqSura
+    @ayat = sura1.xpath('aya').select{|aya| aya['index'].to_i < @nextPageAya.to_i && aya['index'].to_i >= reqAya}
   end
 end
