1d763c557735a575c3270c99a3b7e686f3d2a6b1
Ali Shirvani Jouzdani <aj.shirvani@gmail.com>
2015-02-25 09:26:07 +0330

    simple API call works fine, fixes issue #7

--- app/controllers/quran_controller.rb
+++ app/controllers/quran_controller.rb
@@ -18,7 +18,19 @@ class QuranController < ApplicationController
   end
 
   def fetch_info
-    @result = "some random result" + params[:word]
+    require 'savon'
+    word = params[:word]
+
+    client = Savon.client(wsdl: "https://atks.microsoft.com/Services/SarfService.svc",
+                          endpoint: "https://atks.microsoft.com/Services/SarfService.svc",
+                          namespace: "http://nlptoolkit.cloudapp.net", pretty_print_xml: true,
+                          log: true, log_level: :debug, env_namespace: :soapenv, namespace_identifier: :nlp)
+
+    atks_result = client.call(:analyze_token, message: {"nlp:appId" => ATKS_API_KEY, "nlp:word" => word })
+    @result = atks_result.to_hash[:analyze_token_response][:analyses]
+    logger.info "result:" + @result.to_s
+
+    # @result = "some random result" + params[:word]
     respond_to do |format|
       format.js
     end
