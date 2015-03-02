7c2cc3557df92b30e2935f4f435d63a98ecf637d
Ali Shirvani Jouzdani <aj.shirvani@gmail.com>
2015-03-02 15:16:14 +0330

    quran view single page is ok

--- app/controllers/quran_controller.rb
+++ app/controllers/quran_controller.rb
@@ -7,14 +7,17 @@ class QuranController < ApplicationController
     # retrieve sura number and reqAya from user request
     reqSura = 4
     reqAya = 28
-    @page = quran_data.search("page[sura=\"#{reqSura}\"]").select{|page| page['aya'].to_i <= reqAya}.last['index']
+    pageNode = quran_data.search("page[sura=\"#{reqSura}\"]").select{|page| page['aya'].to_i <= reqAya}.last
+    firstAyaOfPage = pageNode['aya'].to_i
+    @page = pageNode['index']
     nextPage = @page.to_i + 1
     @nextPageSura = quran_data.search("page[index=\"#{nextPage}\"]").first['sura']
     @nextPageAya = quran_data.search("page[index=\"#{nextPage}\"]").first['aya']
 
     sura1 = quran.search("sura[index=\"#{reqSura}\"]")
+    @suraName = sura1.first['name']
     @sura = reqSura
-    @ayat = sura1.xpath('aya').select{|aya| aya['index'].to_i < @nextPageAya.to_i && aya['index'].to_i >= reqAya}
+    @ayat = sura1.xpath('aya').select{|aya| aya['index'].to_i < @nextPageAya.to_i && aya['index'].to_i >= firstAyaOfPage}
   end
 
 
